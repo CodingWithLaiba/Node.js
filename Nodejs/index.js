0de26cca9851ac9e4efd549a86ad4dc75a1f8492
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-29 17:16:09 +0500

    Refactor index.js to use ES module imports for math and name functions

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,2 +1,11 @@
-console.log("Hello world")
-console.log("Hello JS")
+import { add, sub } from "./modules/math.js";
+import { name } from "./modules/name.js";
+
+// const math = require("./modules/math.js");
+
+console.log(name()); //export from name.js
+console.log(add(1, 3)); //export from math.js
+console.log(sub(10, 2));
+
+console.log("Hello world");
+console.log("Hello JS");
