f1cb98143d8ef3612f0437abd0b7f82bf560598e
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-30 16:21:49 +0500

    Refactor request handling in server to improve URL parsing and logging

--- Server/index.js
+++ Server/index.js
@@ -1,20 +1,27 @@
 import http from "http";
 import fs from "fs";
+import url from "url";
 
 const myServer = http.createServer((req, res) => {
+  if (req.url == "/favicon.ico") {
+    return res.end();
+  }
   const log = ` ${Date.now()}: ${req.url} New Request Recevied\n`;
+  const myURL = url.parse(req.url, true);
+  console.log(myURL);
   fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
-        case "/":
-             res.end("HomePage");
-            break;
-     case "/about":
-             res.end("AboutPage");
-            break;
-        default:
-           res.end("404 Page Error");
+    switch (myURL.pathname) {
+      case "/":
+        res.end("HomePage");
+        break;
+      case "/about":
+        const username = myURL.query.myname;
+        const number = myURL.query.num; //http://localhost:5000/about?myname=laiba&num=123
+        res.end("Hi " + `${username}` + "Your number is: " + `${number}`);
+        break;
+      default:
+        res.end("404 Page Error");
     }
-   
   });
 });
 myServer.listen(5000, () => {
