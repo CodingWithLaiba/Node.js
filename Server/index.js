0d685859ab1ddd17c51919f71d8553860ec99e02
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-30 16:48:03 +0500

    Enhance request logging and add signup handling in server

--- Server/index.js
+++ Server/index.js
@@ -6,12 +6,13 @@ const myServer = http.createServer((req, res) => {
   if (req.url == "/favicon.ico") {
     return res.end();
   }
-  const log = ` ${Date.now()}: ${req.url} New Request Recevied\n`;
+  const log = ` ${Date.now()}: ${req.method} ${req.url} New Request Recevied\n`;
   const myURL = url.parse(req.url, true);
-  console.log(myURL);
+  // console.log(myURL);
   fs.appendFile("log.txt", log, (err, data) => {
     switch (myURL.pathname) {
       case "/":
+        if(req.method === "GET")
         res.end("HomePage");
         break;
       case "/about":
@@ -19,6 +20,13 @@ const myServer = http.createServer((req, res) => {
         const number = myURL.query.num; //http://localhost:5000/about?myname=laiba&num=123
         res.end("Hi " + `${username}` + "Your number is: " + `${number}`);
         break;
+        case("/signup"):
+        if(req.method === "GET"){
+          res.end("This is signup form")
+        }
+        else if(req.method === "POST"){
+          res.end("success")
+        }
       default:
         res.end("404 Page Error");
     }
