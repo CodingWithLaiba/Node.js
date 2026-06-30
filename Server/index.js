c5562efa4cefb6612071eb3528451b5ccf0fea18
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-30 15:42:07 +0500

    Implement request logging and response handling in the server

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,21 @@
 import http from "http";
+import fs from "fs";
 
 const myServer = http.createServer((req, res) => {
-  console.log("New Request received");
-  res.end("Hello  from Server");
+  const log = ` ${Date.now()}: ${req.url} New Request Recevied\n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (req.url) {
+        case "/":
+             res.end("HomePage");
+            break;
+     case "/about":
+             res.end("AboutPage");
+            break;
+        default:
+           res.end("404 Page Error");
+    }
+   
+  });
 });
 myServer.listen(5000, () => {
   console.log("Server start");
