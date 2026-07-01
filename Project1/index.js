021a682cc105a2771d9ff9dc424eca99d6d90b1b
Laiba Ijaz <laibaijaz904@gmail.com>
2026-07-01 14:24:30 +0500

    Change PUT to PATCH method for user updates in API route

--- Project1/index.js
+++ Project1/index.js
@@ -19,7 +19,7 @@ app.route("/api/users/:id").get((req, res) => {
   const id = Number(req.params.id);
   const user = users.find((user) => user.id === id);
   return res.json(user);
-}).put((req, res) => {
+}).patch((req, res) => {
  //Eidt with id
  return res.json({status:"pending"})
 }).delete((req, res) => {
